c62cb17f380e3ca7844624d8a55cafcf245ec37b
Michael Sholty <michael.sholty@fanduel.com>
2016-08-15 17:16:40 -0400

    feat: add middleware proxy for wam

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ router.get('/google/places', places);
 app.use('/wam', router);
 app.set('port', process.env.PORT || 3000);
 
-http.createServer(app).listen(app.get('port'), function() {
+http.createServer(app).listen(app.get('port'), start);
+
+function start() {
     console.log('Starting the DNL WAM on port ' + app.get('port'));
-});
+}
 
 function places(req, res) {
     var apiKey = 'AIzaSyDgT571g_5t7Gs7F2QD6SKFy0xNpFbXDaE';
-    var placeId = 'ChIJEwu80FGk6IgRN6w1XJkluZA';//req.params.placeId;
+    var placeId = 'ChIJEwu80FGk6IgRN6w1XJkluZA';
     var url = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' + placeId + '&key=' + apiKey;
 
     var options = {
